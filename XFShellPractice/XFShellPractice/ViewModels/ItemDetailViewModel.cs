89697e801e22394c1d8a265da0c46c41fe53cdef
Shuhey <shuhey.dev@gmail.com>
2020-05-30 19:41:54 +0900

    Add project files.

---
+++ XFShellPractice/XFShellPractice/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XFShellPractice.Models;
+
+namespace XFShellPractice.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
